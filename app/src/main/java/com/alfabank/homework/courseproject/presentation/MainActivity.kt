a738ca1adf859e282673a0eab0cba9033ab12530
k.mordvinova <mordvinovacamilla@gmail.com>
2026-03-03 16:42:51 +0300

    рефафкторинг

--- app/src/main/java/com/alfabank/homework/courseproject/presentation/MainActivity.kt
+++ app/src/main/java/com/alfabank/homework/courseproject/presentation/MainActivity.kt
@@ -4,26 +4,69 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.alfabank.homework.courseproject.MainUiState
 import com.alfabank.homework.courseproject.MainViewModel
 import com.alfabank.homework.courseproject.presentation.ui.theme.CourseProjectTheme
+import com.yandex.mapkit.MapKitFactory
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    private val viewModel: MainViewModel by viewModels()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
         setContent {
             CourseProjectTheme {
                 Surface {
-                    MainScreen(viewModel.uiState,
-                        onRetry = viewModel::retry)
+                    val viewModel: MainViewModel = hiltViewModel()
+                    val uiState = viewModel.uiState.collectAsStateWithLifecycle()
+                    when (val currentState = uiState.value) {
+                        is MainUiState.Loading -> {
+                            Box(
+                                modifier = Modifier.fillMaxSize(),
+                                contentAlignment = Alignment.Center
+                            ) {
+                                CircularProgressIndicator()
+                            }
+                        }
+
+                        is MainUiState.Success -> {
+                            val apiKey = currentState.apiKey
+                            val context = LocalContext.current
+
+                            LaunchedEffect(apiKey) {
+                                MapKitFactory.setApiKey(apiKey)
+                                MapKitFactory.initialize(context)
+                            }
+
+                        }
+
+                        is MainUiState.Error -> {
+                            Box(
+                                modifier = Modifier.fillMaxSize(),
+                                contentAlignment = Alignment.Center
+                            ) {
+                                Button(onClick = viewModel::retry) {
+                                    Text("Ошибка: ${(uiState as MainUiState.Error).message}. Повторить")
+                                }
+                            }
+                        }
+                    }
+                    MainScreen()
                 }
             }
         }
--- app/src/main/java/com/alfabank/homework/courseproject/presentation/MainScreen.kt
+++ app/src/main/java/com/alfabank/homework/courseproject/presentation/MainScreen.kt
@@ -1,10 +1,7 @@
 package com.alfabank.homework.courseproject.presentation
 
 import android.util.Log
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
@@ -15,7 +12,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
@@ -41,41 +37,9 @@ import com.alfabank.homework.courseproject.presentation.homescreen.components.Fe
 import com.alfabank.homework.courseproject.presentation.homescreen.eventScreen.EventDetailScreen
 import com.alfabank.homework.courseproject.presentation.mapScreen.MapScreen
 import com.alfabank.homework.courseproject.presentation.profilescreen.ProfileScreen
-import com.yandex.mapkit.MapKitFactory
-import kotlinx.coroutines.flow.StateFlow
 
-@Suppress("NonSkippableComposable")
 @Composable
-fun MainScreen(
-    uiStateFlow: StateFlow<MainUiState>,
-    onRetry: () -> Unit
-) {
-    val uiState by uiStateFlow.collectAsStateWithLifecycle()
-    when (uiState) {
-        is MainUiState.Loading -> {
-            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                CircularProgressIndicator()
-            }
-        }
-        is MainUiState.Success -> {
-            val apiKey = (uiState as MainUiState.Success).apiKey
-            val context = LocalContext.current
-
-            LaunchedEffect(apiKey) {
-                MapKitFactory.setApiKey(apiKey)
-                MapKitFactory.initialize(context)
-            }
-
-        }
-        is MainUiState.Error -> {
-            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                Button(onClick = onRetry) {
-                    Text("Ошибка: ${(uiState as MainUiState.Error).message}. Повторить")
-                }
-            }
-        }
-    }
-
+fun MainScreen() {
     val viewModel: EventViewModel = viewModel()
     val queries by viewModel.query.collectAsStateWithLifecycle()
 
