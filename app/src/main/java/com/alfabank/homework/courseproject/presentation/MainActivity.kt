0bb77af4a370136d515200a90f12cc03b9f09ff4
k.mordvinova <mordvinovacamilla@gmail.com>
2026-03-03 17:07:27 +0300

    рефафкторинг

--- app/src/main/java/com/alfabank/homework/courseproject/presentation/MainActivity.kt
+++ app/src/main/java/com/alfabank/homework/courseproject/presentation/MainActivity.kt
@@ -11,10 +11,8 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.alfabank.homework.courseproject.MainUiState
@@ -46,11 +44,10 @@ class MainActivity : ComponentActivity() {
 
                         is MainUiState.Success -> {
                             val apiKey = currentState.apiKey
-                            val context = LocalContext.current
 
                             LaunchedEffect(apiKey) {
                                 MapKitFactory.setApiKey(apiKey)
-                                MapKitFactory.initialize(context)
+                                MapKitFactory.initialize(this@MainActivity)
                             }
 
                         }
